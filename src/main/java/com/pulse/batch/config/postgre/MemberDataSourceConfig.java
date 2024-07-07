bfd602ae11d7683a715da219a7ccd3c9c3756d1e
wlsdks <wlsdks12@naver.com>
2024-07-07 20:34:30 +0900

    [PULB-2] :sparkles: DB 설정 (3개의 msa서버)

---
+++ src/main/java/com/pulse/batch/config/postgre/MemberDataSourceConfig.java
@@ -0,0 +1,65 @@
+package com.pulse.batch.config.postgre;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        basePackages = "com.pulse.batch.repository.member",
+        entityManagerFactoryRef = "memberEntityManagerFactory",
+        transactionManagerRef = "memberTransactionManager"
+)
+public class MemberDataSourceConfig {
+
+    @Primary
+    @Bean(name = "memberDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.member")
+    public DataSource memberDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "memberEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean memberEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("memberDataSource") DataSource dataSource
+    ) {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "create");
+
+        return builder
+                .dataSource(dataSource)
+                .packages("com.pulse.batch.entity.member")
+                .persistenceUnit("member")
+                .properties(properties)
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "memberTransactionManager")
+    public PlatformTransactionManager memberTransactionManager(
+            @Qualifier("memberEntityManagerFactory") EntityManagerFactory memberEntityManagerFactory
+    ) {
+        return new JpaTransactionManager(memberEntityManagerFactory);
+    }
+
+}
+
---
+++ src/main/java/com/pulse/batch/config/postgre/SocialGraphDataSourceConfig.java
@@ -0,0 +1,61 @@
+package com.pulse.batch.config.postgre;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        basePackages = "com.pulse.batch.repository.social",
+        entityManagerFactoryRef = "socialGraphEntityManagerFactory",
+        transactionManagerRef = "socialGraphTransactionManager"
+)
+public class SocialGraphDataSourceConfig {
+
+    @Bean(name = "socialGraphDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.social")
+    public DataSource socialGraphDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "socialGraphEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean socialGraphEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("socialGraphDataSource") DataSource dataSource
+    ) {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "create");
+
+        return builder
+                .dataSource(dataSource)
+                .packages("com.pulse.batch.entity.social")
+                .persistenceUnit("socialGraph")
+                .properties(properties)
+                .build();
+    }
+
+    @Bean(name = "socialGraphTransactionManager")
+    public PlatformTransactionManager socialGraphTransactionManager(
+            @Qualifier("socialGraphEntityManagerFactory") EntityManagerFactory socialGraphEntityManagerFactory
+    ) {
+        return new JpaTransactionManager(socialGraphEntityManagerFactory);
+    }
+
+
+}
