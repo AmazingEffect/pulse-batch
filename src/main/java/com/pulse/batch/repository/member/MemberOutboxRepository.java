3008115bf44356fbc19cd5a5d569d411b78b1329
wlsdks <wlsdks12@naver.com>
2024-07-13 16:14:04 +0900

    [PULB-3] :sparkles: 각 엔티티별 repository 추가

---
+++ src/main/java/com/pulse/batch/repository/member/MemberOutboxRepository.java
@@ -0,0 +1,13 @@
+package com.pulse.batch.repository.member;
+
+import com.pulse.batch.entity.MessageStatus;
+import com.pulse.batch.entity.member.MemberOutbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberOutboxRepository extends JpaRepository<MemberOutbox, Long> {
+
+    List<MemberOutbox> findByStatus(MessageStatus status);
+
+}
---
+++ src/main/java/com/pulse/batch/repository/social/SocialGraphOutboxRepository.java
@@ -0,0 +1,13 @@
+package com.pulse.batch.repository.social;
+
+import com.pulse.batch.entity.MessageStatus;
+import com.pulse.batch.entity.social.SocialGraphOutbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SocialGraphOutboxRepository extends JpaRepository<SocialGraphOutbox, Long> {
+
+    List<SocialGraphOutbox> findByStatus(MessageStatus status);
+
+}
