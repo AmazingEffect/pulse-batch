b484bd043fb3300c509a935756b85b886ad4a1f2
wlsdks <wlsdks12@naver.com>
2024-07-13 19:47:18 +0900

    [PUBL-4] :recycle: 배치 스케쥴러 try-catch 추가

--- src/main/java/com/pulse/batch/schedule/ReIssueEventBatchScheduler.java
+++ src/main/java/com/pulse/batch/schedule/ReIssueEventBatchScheduler.java
@@ -1,9 +1,16 @@
 package com.pulse.batch.schedule;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,6 +18,7 @@ import org.springframework.stereotype.Component;
 /**
  * Spring Scheduler를 사용하여 5분마다 배치 잡을 실행하도록 설정
  */
+@Slf4j
 @RequiredArgsConstructor
 @Component
 @EnableScheduling
@@ -21,27 +29,49 @@ public class ReIssueEventBatchScheduler {
     private final Job processContentOutboxJob;
     private final Job processSocialGraphOutboxJob;
 
+
     @Scheduled(fixedRate = 300000) // 5분(300000ms)마다 실행
-    public synchronized void runMemberJob() throws Exception {
-        jobLauncher.run(processMemberOutboxJob, new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters());
+    public synchronized void runMemberJob() {
+        try {
+            jobLauncher.run(processMemberOutboxJob, new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters());
+        } catch (JobExecutionAlreadyRunningException |
+                 JobRestartException |
+                 JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            log.error("Error running processMemberOutboxJob", e);
+        }
     }
 
 
     @Scheduled(fixedRate = 300000) // 5분(300000ms)마다 실행
-    public synchronized void runContentJob() throws Exception {
-        jobLauncher.run(processContentOutboxJob, new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters());
+    public synchronized void runContentJob() {
+        try {
+            jobLauncher.run(processContentOutboxJob, new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters());
+        } catch (JobExecutionAlreadyRunningException |
+                 JobRestartException |
+                 JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            log.error("Error running processContentOutboxJob", e);
+        }
     }
 
 
     @Scheduled(fixedRate = 300000) // 5분(300000ms)마다 실행
-    public synchronized void runSocialGraphJob() throws Exception {
-        jobLauncher.run(processSocialGraphOutboxJob, new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters());
+    public synchronized void runSocialGraphJob() {
+        try {
+            jobLauncher.run(processSocialGraphOutboxJob, new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters());
+        } catch (JobExecutionAlreadyRunningException |
+                 JobRestartException |
+                 JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            log.error("Error running processSocialGraphOutboxJob", e);
+        }
     }
 
 }
